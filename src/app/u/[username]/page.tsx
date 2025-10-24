c26f275b45f238e8de92bbf2d1b1aa58cf72012d
Kapilansh Patil <kapilansh.patil.etrx@ghrce.raisoni.net>
2025-10-25 03:57:09 +0530

    Implemented user messaging feature

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,9 +1,58 @@
+"use client"
+
+import { useState } from "react";
+import { useParams } from "next/navigation";
 
 
-export const PublicPage = () => {
+export default function UserPage({params}) {
+
+    const { username } = useParams();
+
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [status, setStatus] = useState("");
+
+    const handleSend = async () => {
+
+        if(!message.trim()) {
+            alert("Message cannot be empty");
+            return;
+        }
+        setLoading(true);
+        const response = await fetch("/api/messages", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({ username, content: message })
+        })
+
+
+        if(response.ok) {
+            alert("✅ Message sent successfully!");
+            setMessage("");
+        }
+        else {
+            alert("User not found")
+            setStatus("❌ User not found")
+        }
+
+        setLoading(false)
+    }
+
     return (
-        <div>
-            Welcome to username Page
+        <div >
+            <h1>Send an annonymous message to user</h1>
+            
+            <textarea
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Write your message..."
+            />
+
+            <button onClick={handleSend}>
+                {loading ? "Sending..." : "Send Message"}
+            </button>
+
+            {status && <p>{status}</p>}
         </div>
     )
 }
